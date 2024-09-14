2d7b7dc67ef58609ddc6350a0accf42bf5e982ba
ShagunRathor <142313879+ShagunRathor@users.noreply.github.com>
2024-09-14 18:37:57 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,199 @@
+// console.log()
+// console.log("hello")
+// document.write("hello")
+// window.alert("error")
+
+// var a=2;
+
+// console.log(a)
+
+// let b=45;
+// console.log(b)
+
+// const c=89;
+// console.log(c)
+
+// var aa=2;
+// let bb=56;
+// const cc=aa+bb;
+// console.log(cc)
+
+// var a=34;
+// let s=5;
+// const t=a*s;
+// console.log(t)
+
+// var a=290
+// let d=3
+// const e=a/d
+// console.log(e)
+
+// var a=300
+// let f=100
+// const g=a-f
+// console.log(g)
+
+// var a=10.30
+// console.log(typeof a)
+
+// var a= true
+// console.log(typeof a)
+
+// var a=null
+// console.log(typeof a)
+
+// var a=undefined
+// console.log(typeof a)
+
+// var a="hello"
+// console.log(typeof a)
+
+// let a=20;
+// let b=30;
+// let c=40;
+
+// if (a=20){
+//     console.log("This is true statement")
+// }
+
+// else {
+//     console.log("This is false statement")
+// }
+
+// if (a>b)
+// {
+//     console.log("a is less than b")
+// }
+// else {
+//     console.log("b is greater than a")
+// }
+// if(b<c){
+//     console.log("b is less than c")
+// }
+// else{
+//     console.log("c is greater than a")
+// }
+// let d = 5;
+// let e = 10;
+
+// d = d + e;
+// e = d - e;
+// d = d - e;
+
+// console.log(d);
+// console.log(e);
+
+
+// for (let i=9; i<=39; i++){
+//     console.log(i)
+// }
+
+// let month =13;
+
+// switch(month){
+//     case 1:
+//     console.log("January")
+//     break;
+
+//     case 2:
+//         console.log("February");
+//         break;
+
+//         case 3:
+//             console.log("March");
+//             break;
+
+//             case 4:
+//                 console.log("April");
+//                 break;
+
+//                 case 5:
+//                     console.log("May");
+//                     break;
+
+//                     case 6:
+//                         console.log("June");
+//                         break;
+
+//                         case 7:
+//                             console.log("July");
+//                             break;
+
+//                             case 8:
+//                                 console.log("August");
+//                                 break;
+
+//                                 case 9:
+//                                     console.log("September");
+//                                     break;
+
+//                                     case 10:
+//                                         console.log("October");
+//                                         break;
+
+//                                         case 11:
+//                                             console.log("November");
+//                                             break;
+
+//                                             case 12:
+//                                                 console.log("December");
+//                                                 break;
+
+//                                                 default:
+//                                                     console.log("Enter valid month");
+//                                                     break;
+
+// }
+
+
+// let per = prompt("Enter your %age");
+
+// // let per = 70;
+// if (per>=80 && per<=100){
+//     console.log("You are in merit");
+// }
+
+// else if(per >=70 && per<=80){
+//     console.log("you are in first division");
+// }
+// else if(per>=60 && per<=70){
+//     console.log("you are in second division");
+// }
+
+// else if(per>= 50 && per<=60){
+//     console.log("you are in third division");
+// }
+
+// else if(per>=33 && per<50){
+//     console.log("you are pass");
+// }
+
+// else if(per<=33){
+//     console.log("you  are fail");
+// }
+
+
+// let arry =[10,20,30,40,50,60,70];
+// let sum= 0;
+// for(let i=0; i<=6; i++){
+//     document.write(arry[i] + " "  +"<br>");
+//     sum= sum + arry[i];
+// }
+//     document.write ("Sum is" + sum +" " +"<br>") 
+
+// for (let i = 1; i <= 7; i++) {
+//  document.write('*'.repeat(i));
+//   document.write("<br>");
+// }
+// document.write("<br>");
+// for (let i=7; i>=1; i--){
+//     document.write('*'.repeat(i));
+//     document.write("<br>");
+// }
+
+
+// let number = prompt("Enter a number");
+
+// for(i=1;i<=10;i++){
+//     console.log(number+"x"+i+"="+number*i);
+// } 
